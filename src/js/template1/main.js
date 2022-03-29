c1d7ddec285e2a67fa11856bd7d58750e3436e4d
Shakirov_Arthur <Wolverane32@gmail.com>
2022-03-29 10:37:55 +0500

    кнопка работает

---
+++ src/js/template1/main.js
@@ -0,0 +1,6 @@
+let editor = document.querySelector(".editor");
+let display_btn = document.querySelector(".icon-expand");
+
+display_btn.addEventListener("click", () => {
+    editor.style.display = "none";
+})
